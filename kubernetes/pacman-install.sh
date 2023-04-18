9cf9fc17421505e482336a509229d1db69f64fb6
Duck <70207455+entlein@users.noreply.github.com>
2023-04-18 15:19:16 +0200

    Features/lecture7 (#21)
    * ingress corrected and tested with latest rancher cluster config
    
    * cert manager annotation was missing

--- kubernetes/pacman-install.sh
+++ kubernetes/pacman-install.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 # Taken from this blog: https://vzilla.co.uk/vzilla-blog/building-the-home-lab-kubernetes-playground-part-9
-
+export ip_whitelist_range='128.130.0.0/15'
+export dnsa='caas-0011'
+export env='dev'
 echo "Installing your PacMan app on K8s"
 envsubst < persistentvolumeclaim/mongo-pvc.txt.yaml > persistentvolumeclaim/mongo-pvc.yaml
 envsubst < security/secret.txt.yaml > security/secret.yaml
 envsubst < ingress/ingress.txt.yaml > ingress/ingress.yaml
 #I took out the priviledged parts here, this is not required, we can discuss this
-#kubectl apply -n $pacman -f security/rbac.yaml
-kubectl apply -n $pacman -f security/secret.yaml
-kubectl apply -n $pacman -f persistentvolumeclaim/mongo-pvc.yaml
-kubectl apply -n $pacman -f configmap/pacman-mongo-common-scripts.yaml
-kubectl apply -n $pacman -f deployments/mongo-deployment.yaml
-while [ "$(kubectl get pods -l=name='mongo' -n $pacman -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
-   sleep 5
-   echo "Waiting for mongo pod to change to running status"
-done
-kubectl apply -n $pacman -f deployments/pacman-deployment.yaml
-kubectl apply -n $pacman -f services/mongo-service.yaml
-kubectl apply -n $pacman -f services/pacman-service.yaml
-kubectl apply -n $pacman -f ingress/ingress.yaml
+# #kubectl apply -n $pacman -f security/rbac.yaml
+# kubectl apply -n $pacman -f security/secret.yaml
+# kubectl apply -n $pacman -f persistentvolumeclaim/mongo-pvc.yaml
+# kubectl apply -n $pacman -f configmap/pacman-mongo-common-scripts.yaml
+# kubectl apply -n $pacman -f deployments/mongo-deployment.yaml
+# while [ "$(kubectl get pods -l=name='mongo' -n $pacman -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
+#    sleep 5
+#    echo "Waiting for mongo pod to change to running status"
+# done
+# kubectl apply -n $pacman -f deployments/pacman-deployment.yaml
+# kubectl apply -n $pacman -f services/mongo-service.yaml
+# kubectl apply -n $pacman -f services/pacman-service.yaml
+# kubectl apply -n $pacman -f ingress/ingress.yaml
